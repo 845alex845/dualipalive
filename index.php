cd45e0498567ad24e928b31a03ae78952eab119a
845alex845 <alexunmsm2016@gmail.com>
2020-04-08 00:50:28 -0500

    bootstrap

---
+++ index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;600&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md sticky-md bd-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="image/logo.png" class="logo-brand" alt="logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item" id="home">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item" id="portfolio">
+                        <a class="nav-link" href="#">Portfolio</a>
+                    </li>
+                    <li class="nav-item" id="about-us">
+                        <a class="nav-link " href="#">About us</a>
+                    </li>
+                    <li class="nav-item" id="pricing">
+                        <a class="nav-link " href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item" id="testimonial">
+                        <a class="nav-link " href="#">Testimonial</a>
+                    </li>
+                    <li class="nav-item" id="contact">
+                        <a class="nav-link " href="#">Contact</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    <section id="hero">
+        <div class="container">
+            <div class="content-center topmargin-lg">
+                <h1>Web Design and Development</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam facere saepe natus iusto repudiandae doloremque maiores debitis modi hic enim?</p>
+                <a href="#" class="btn btn-light topmargin-sm">Explore now <i class="fas fa-play-circle"></i>   </a>
+            </div>
+        </div>
+    </section>
+    <section id="portfolio" class="bg-port">
+        <div class="container">
+            <div class="content-center">
+                <h2>Lorem ipsum dolor sit amet consectetur
+                    <b>adipisicing elit.</b>
+                </h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque nesciunt a asperiores, temporibus quibusdam dolore? Odit eius quis culpa eum quasi exercitationem. Praesentium, illum ex commodi vero exercitationem culpa laboriosam
+                    animi ducimus ipsum dolore dolorem.</p>
+            </div>
+            <div class="row justify-content-center align-items-center">
+                <div class="col-md-4 ">
+                    <div class="portfolio-container">
+                        <div class="portfolio-details">
+                            <a href="#">
+                                <h2>Don't start now</h2>
+                            </a>
+                            <a href="">
+                                <p>Primer album</p>
+                            </a>
+                        </div>
+                        <img src="image/dua1.jpg" class="img-fluid" alt="portafolio 1">
+                    </div>
+                </div>
+                <div class="col-md-4 ">
+                    <div class="portfolio-container">
+                        <div class="portfolio-details">
+                            <a href="#">
+                                <h2>Be the One</h2>
+                            </a>
+                            <a href="">
+                                <p>Segundo album</p>
+                            </a>
+                        </div>
+                        <img src="image/dua2.jpg" class="img-fluid" alt="portafolio 1">
+                    </div>
+                </div>
+                <div class="col-md-4 ">
+                    <div class="portfolio-container">
+                        <div class="portfolio-details">
+                            <a href="#">
+                                <h2>IDGAF</h2>
+                            </a>
+                            <a href="">
+                                <p>Tercer album</p>
+                            </a>
+                        </div>
+                        <img src="image/dua3.jpg" class="img-fluid" alt="portafolio 1">
+                    </div>
+                </div>
+                <div class="col-md-4 ">
+                    <div class="portfolio-container">
+                        <div class="portfolio-details">
+                            <a href="#">
+                                <h2>Don't start now</h2>
+                            </a>
+                            <a href="">
+                                <p>Primer album</p>
+                            </a>
+                        </div>
+                        <img src="image/dua6.jpg" class="img-fluid" alt="portafolio 1">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="portfolio-container">
+                        <div class="portfolio-details">
+                            <a href="#">
+                                <h2>Don't start now</h2>
+                            </a>
+                            <a href="">
+                                <p>Primer album</p>
+                            </a>
+                        </div>
+                        <img src="image/dua1.jpg" class="img-fluid" alt="portafolio 1">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="portfolio-container">
+                        <div class="portfolio-details">
+                            <a href="#">
+                                <h2>Be the One</h2>
+                            </a>
+                            <a href="">
+                                <p>Segundo album</p>
+                            </a>
+                        </div>
+                        <img src="image/dua2.jpg" class="img-fluid" alt="portafolio 1">
+                    </div>
+                </div>
+            </div>
+            <div class="text-center topmargin-sm">
+                <p>Lorem ipsum dolor sit amet . Animi, suscipit.</p>
+                <a href="#" class="text-dark">click aquí</a>
+            </div>
+        </div>
+    </section>
+    <section id="about-us" class="bg-about-us">
+        <div class="container">
+            <div class="content-center">
+                <h2>Lorem ipsum dolor sit amet consectetur
+                    <b>Dua en concierto</b>
+                </h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque nesciunt a asperiores, temporibus quibusdam dolore? Odit eius quis culpa eum quasi exercitationem. Praesentium, illum ex commodi vero exercitationem culpa laboriosam
+                    animi ducimus ipsum dolore dolorem.</p>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4 px-2">
+                    <div class="member-container borde">
+                        <div class="member-details">
+                            <h5>California , EE.UU.</h5>
+                            <span>Sep , Monday 01</span>
+                            <ul class="list-inline">
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-whatsapp"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-facebook"></i></a></li>
+                            </ul>
+                        </div>
+                        <img src="image/concierto1.jpg" class="img-fluid" alt="photo01">
+                    </div>
+                </div>
+                <div class="col-md-4 px-2">
+                    <!-- RETO : Colocarlo como carrusel -->
+                    <div class="member-container borde">
+                        <div class="member-details">
+                            <h5>Manchester, England</h5>
+                            <span>May , Tuesday 14 </span>
+                            <ul class="list-inline">
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-whatsapp"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-facebook"></i></a></li>
+                            </ul>
+                        </div>
+                        <img src="image/concierto2.jpg" class="img-fluid" alt="photo01">
+                    </div>
+                </div>
+                <div class="col-md-4 px-2">
+                    <div class="member-container borde">
+                        <div class="member-details">
+                            <h5>Paris ,France</h5>
+                            <span>Oct , Saturday 19</span>
+                            <ul class="list-inline">
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-whatsapp"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fab fa-facebook"></i></a></li>
+                            </ul>
+                        </div>
+                        <img src="image/concierto3.jpg" class="img-fluid" alt="photo01">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="pricing">
+        <div class="container">
+            <div class="content-center topmargin-lg">
+                <h1>Web Design and Development</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam facere saepe natus iusto repudiandae doloremque maiores debitis modi hic enim?</p>
+                <a href="#" class="btn btn-light topmargin-sm">Explore now <i class="fas fa-play-circle"></i>   </a>
+            </div>
+            <div class="row justify-content-around align-items-center">
+                <div class="col-md-5">
+                    <div class="pricing-container">
+                        <div class="plans plan-light">
+                            <h4>Starter Solution</h4>
+                            <h2>$89<span>/per month</span></h2>
+                            <h4>Per 1 Million Data points</h4>
+                            <ul class="topmargin-xs">
+                                <li>Integratiion with Google Analytics</li>
+                                <li>Online Payments</li>
+                                <li>Solution Architect</li>
+                                <li>Dedicated Support</li>
+                            </ul>
+                            <a href="#" class="btn btn-dark d-block topmargin-sm"> Star now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5 ">
+                    <div class="pricing-container">
+                        <div class="plans plan-dark">
+                            <h4>Starter Solution</h4>
+                            <h2>$139<span>/per month</span></h2>
+                            <h4>Per 1 Million Data points</h4>
+                            <ul class="topmargin-xs">
+                                <li>Integratiion with Google Analytics</li>
+                                <li>Online Payments</li>
+                                <li>Solution Architect</li>
+                                <li>Dedicated Support</li>
+                            </ul>
+                            <a href="#" class="btn btn-dark d-block topmargin-sm"> Star now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://kit.fontawesome.com/b84a853aee.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
